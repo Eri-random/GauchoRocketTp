dfe19ff522d5b6000f564acddd5b26896a4e6776
Gaston Santos <gastons525@gmail.com>
2022-06-02 13:16:33 -0300

    cagada

--- controller/VuelosController.php
+++ controller/VuelosController.php
@@ -8,43 +8,67 @@ class VuelosController {
 
     public function __construct($printer, $vuelosModel) {
         $this->printer = $printer;
-        
         $this->vuelosModel = $vuelosModel;
 
     }
 
-    public function show() {
-
-        echo $this->printer->render("registroView.html");
-    }
     public function buscarVuelos (){
-        $buscar = $_POST["buscar"];
+        $origen = $_POST["origen"];
+        $destino = $_POST["destino"];
+        $fecha = $_POST["fecha"];
+
 
         if(!isset($_SESSION["logueado"]) || (isset($_SESSION["logueado"]) && !$_SESSION["logueado"] )){
-            $data["esNada"] = "esNada";   
-            $data["viajes"] = $this->vuelosModel->buscarVuelos($buscar);
             
-            echo $this->printer->render("homeView.html", $data);
-            exit();
-           } 
-             else if (isset($_SESSION["esAdmin"])) {
-    
-                $data["esAdmin"] = $_SESSION["esAdmin"];
-                $data["viajes"] = $this->vuelosModel->buscarVuelos($buscar);
-                echo $this->printer->render("homeView.html", $data);
-                exit();
-            }else if(isset($_SESSION["esClient"])){
-            
-                $data["esClient"] = $_SESSION["esClient"];
-                $data["viajes"] = $this->vuelosModel->buscarVuelos($buscar);
-                echo $this->printer->render("homeView.html", $data);
-                exit();
+        if($origen == "" && $destino == "" && $fecha == ""){
+            $data["vacio"] = true;//Esta vacio Cartelito    
+
+            echo $this->printer->render("HomeView.html", $data);
+        }else{
+            $data["viajes"] = $this->vuelosModel->buscarVuelos($origen,$destino,$fecha);
+            if(empty($data["viajes"]) ){
+                $data["error"] = true;//No se encontro resultado cartelito
+            }else{
+                $data["error"] = false;
             }
+            
+            echo $this->printer->render("HomeView.html", $data);
+        }
 
+    } else if (isset($_SESSION["esAdmin"])) {
+        $data["esAdmin"] = $_SESSION["esAdmin"];
+        if($origen == "" && $destino == "" && $fecha == ""){
+            $data["vacio"] = true;//Esta vacio Cartelito    
+
+            echo $this->printer->render("HomeView.html", $data);
+        }else{
+            $data["viajes"] = $this->vuelosModel->buscarVuelos($origen,$destino,$fecha);
+            if(empty($data["viajes"]) ){
+                $data["error"] = true;//No se encontro resultado cartelito
+            }else{
+                $data["error"] = false;
+            }
+            
+            echo $this->printer->render("HomeView.html", $data);
+        }
+    }else if (isset($_SESSION["esClient"])){
+        $data["esClient"] = $_SESSION["esClient"];
+        if($origen == "" && $destino == "" && $fecha == ""){
+            $data["vacio"] = true;//Esta vacio Cartelito    
+
+            echo $this->printer->render("HomeView.html", $data);
+        }else{
+            $data["viajes"] = $this->vuelosModel->buscarVuelos($origen,$destino,$fecha);
+            if(empty($data["viajes"]) ){
+                $data["error"] = true;//No se encontro resultado cartelito
+            }else{
+                $data["error"] = false;
+            }
+            
+            echo $this->printer->render("HomeView.html", $data);
     }
-
-  
-
-
+    }
 }
+}
+
 
